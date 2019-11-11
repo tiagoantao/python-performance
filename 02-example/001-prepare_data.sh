aa111f83fca3fde817a02aa86476df3f642da085
Tiago Rodrigues Antao <tiagoantao@gmail.com>
2019-11-11 16:47:56 -0500

    plink bug on founders

--- 02-example/001-prepare_data.sh
+++ 02-example/001-prepare_data.sh
@@ -7,6 +7,6 @@ wget http://zzz.bwh.harvard.edu/plink/dist/hapmap_r23a.zip
 wget http://zzz.bwh.harvard.edu/plink/dist/hapmap.pop
 unzip hapmap_r23a.zip
 rm hapmap_r23a.zip
-plink --bfile hapmap_r23a --out clean_hapmap --make-bed --make-founders
+plink --bfile hapmap_r23a --out clean_hapmap --make-bed --filter-founders
 plink --bfile clean_hapmap --out my_hapmap  --recode
 rm hapmap* clean_hapmap.*
