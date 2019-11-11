f15af58b45ac87c45d738dc8b14dea907d74b6d4
Tiago Rodrigues Antao <tiagoantao@gmail.com>
2019-11-11 14:52:38 -0500

    Downloading data for chapter 2

---
+++ 02-example/001-prepare_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Downloads HapMap data, removes non-founders, converts to text format
+mkdir -p scratch
+cd scratch
+wget http://zzz.bwh.harvard.edu/plink/dist/hapmap_r23a.zip
+wget http://zzz.bwh.harvard.edu/plink/dist/hapmap.pop
+unzip hapmap_r23a.zip
+rm hapmap_r23a.zip
+plink --bfile hapmap_r23a --out clean_hapmap --make-bed --make-founders
+plink --bfile clean_hapmap --out my_hapmap  --recode
+rm hapmap* clean_hapmap.*
