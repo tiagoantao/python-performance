f6281d4d8919c7cc4f5d76e845cb0789ee8d224e
Tiago Rodrigues Antao <tiagoantao@gmail.com>
2019-11-13 08:13:26 -0500

    convert data bug

--- 02-example/001-prepare_data.sh
+++ 02-example/001-prepare_data.sh
@@ -8,5 +8,5 @@ wget http://zzz.bwh.harvard.edu/plink/dist/hapmap.pop
 unzip hapmap_r23a.zip
 rm hapmap_r23a.zip
 plink --bfile hapmap_r23a --out clean_hapmap --make-bed --filter-founders
-plink --bfile clean_hapmap --out my_hapmap  --recode
+plink --bfile clean_hapmap --recode transpose --out my_hapmap
 rm hapmap* clean_hapmap.*
