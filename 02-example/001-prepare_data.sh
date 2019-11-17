a0e8337bb1183b10dbbb4a8e5faa4a438e747000
Tiago Rodrigues Antao <tiago@tiago.org>
2019-11-17 11:04:37 -0500

    data download

--- 02-example/001-prepare_data.sh
+++ 02-example/001-prepare_data.sh
@@ -4,9 +4,9 @@
 mkdir -p scratch
 cd scratch
 wget http://zzz.bwh.harvard.edu/plink/dist/hapmap_r23a.zip
-wget http://zzz.bwh.harvard.edu/plink/dist/hapmap.pop
+wget -o hapmap.pop http://zzz.bwh.harvard.edu/plink/dist/hapmap.pop
 unzip hapmap_r23a.zip
 rm hapmap_r23a.zip
 plink --bfile hapmap_r23a --out clean_hapmap --make-bed --filter-founders
 plink --bfile clean_hapmap --recode transpose --out my_hapmap
-rm hapmap* clean_hapmap.*
+rm hapmap_* clean_hapmap.*
