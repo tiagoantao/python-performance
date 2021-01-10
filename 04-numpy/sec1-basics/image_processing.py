8eadb7f96e1c18cfefa1a2a91fd221aaded098a4
Tiago Antao <tiago@tiago.org>
2021-01-10 14:29:31 -0500

    More verbose output

--- 04-numpy/sec1-basics/image_processing.py
+++ 04-numpy/sec1-basics/image_processing.py
@@ -4,18 +4,19 @@ import numpy as np
 from PIL import Image
 
 image = Image.open("../manning-logo.png").convert("L")
-print(image.size)
+print("Image size:", image.size)
 width, height = image.size
 #  https://numpy.org/doc/stable/reference/arrays.interface.html
 # print(image.__array_interface__)
-image_arr = np.array(image)  # Explain why this works
-print(image_arr.shape, image_arr.dtype)    # Shape is flipped of size
-print(image_arr.size * image_arr.itemsize, image_arr.nbytes)
-print(sys.getsizeof(image_arr))
+image_arr = np.array(image)
+print("Array shape, array type:", image_arr.shape, image_arr.dtype)
+print("Array size * item size: ", image_arr.nbytes)
+print("Array nbytes:", image_arr.nbytes)
+print("sys.getsizeof:", sys.getsizeof(image_arr))
 flipped_from_view = np.flipud(image_arr)
 flipped_from_copy = np.flipud(image_arr).copy()
 print(image_arr.strides, flipped_from_view.strides)
-image_arr[:, :width//2] = 0  # A form of broadcasting?
+image_arr[:, :width//2] = 0
 removed = Image.fromarray(image_arr, "L")
 image.save("image.png")
 removed.save("removed.png")
@@ -30,7 +31,7 @@ print(image_arr == flipped_from_view.base)
 print(np.shares_memory(image_arr, flipped_from_copy),
       np.shares_memory(image_arr, flipped_from_view))
 
-
+print(1)
 print(flipped_from_copy.base, flipped_from_view.base)
 print(flipped_from_view.base is image_arr)
 print(flipped_from_view.base == image_arr)
