bfec0851d61d3933b831c8a392306abe38586b6d
Tiago Antao <tiago@tiago.org>
2021-05-31 20:25:18 -0400

    dummy zip for the FSSPEC example

--- 08-persistence/sec1-fsspec/git_https.py
+++ 08-persistence/sec1-fsspec/git_https.py
@@ -3,7 +3,9 @@ from fsspec.implementations.github import GithubFileSystem
 
 fs = GithubFileSystem("tiagoantao", "python-performance")
 print(fs.ls(""))
-print(fs.ls("07-pandas/sec2-speed"))
+print(fs.ls("08-persistence/sec1-fsspec"))
+for root, dirs, files in fs.walk(""):
+    print(root)
 #fsa = fsspec.get_mapper("github://tiagoantao:python-performance@")
 #print(fsa)
 # fs = fsspec.open("git_https.py")
